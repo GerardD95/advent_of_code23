0cd9f15ee61048c1d2fecc1a2e8a4e4aba4718ed
Gerard Draadjer <gdraadjer@xccelerated.io>
2023-12-01 22:52:09 +0100

    fixed the program because my previous interpretation of the assignment wasnt right. i thought that couldnt use chars twice so eightwo would be 8wo, but the right answer is 82

--- day1/solution.py
+++ day1/solution.py
@@ -17,55 +17,58 @@ def part1(data):
 def part2(text):
   # Define a mapping for the words to their numerical representations
   number_mapping = {
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9'
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
   }
   
   char_index = 0
-  while char_index <= len(text):
+  numbers = []
+  while char_index < len(text):
+    if text[char_index].isdigit():
+      numbers.append(int(text[char_index]))
+      char_index += 1
+      continue
+
     for key, value in number_mapping.items():
       text_selection = text[char_index:char_index + len(key)]
       if text_selection == key:
-        text = text.replace(text_selection, value)
+        numbers.append(value)
         char_index += 1
         break
       # check if key value pair is the lat in number_mapping
       elif key == list(number_mapping.keys())[-1]:
         char_index += 1
   
-  return text
+  return numbers
 
 
-data = [
-  # 'two1nine',
-  'eightwothree',
-  # 'abcone2threexyz',
-  # 'xtwone3four',
-  # '4nineeightseven2',
-  # 'zoneight234',
-  # '7pqrstsixteen'
-]
+# data = [
+#   'two1nine',
+#   '2eightwothree',
+#   'abcone2threexyz',
+#   'xtwone3four',
+#   '4nineeightseven2',
+#   'zoneight234',
+#   '7pqrstsixteen'
+# ]
 
 # Convert written numbers to digits
-res = [part2(line) for line in data]
-with open("data/output.txt", "w") as f:
-  for line in res:
-    f.write(f"{line}")
-
-with open("data/numbers.txt", 'w') as f:
-  for line in part1(res):
-    f.write(f"{line}\n")
+res = [part2(text_str) for text_str in data]
 
 # get all numbers from text
-num_lst = part1(res)
+# num_lst = part1(res)
 
 # sum all numbers
-print(res)
-print(f"Sum of all numbers: {sum(num_lst)}")
+res = [int(f"{i[0]}{i[-1]}") for i in res]
+print(sum(res))
+
+
+# [int(f"{i[0]}{i[-1]}") for i in only_numbers]
+# print(f"Sum of all numbers: {sum(num_lst)}")
